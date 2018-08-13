2a232697e3d63a100adae74424c360d1ed315483
Lukasz Wojciechowski - Codete <lukasz.wojciechowski@codete.com>
2018-08-13 11:55:39 +0200

    Added native_bot branch to the list

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/client/BitbucketCloudApiClient.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/client/BitbucketCloudApiClient.java
@@ -414,7 +414,7 @@ public class BitbucketCloudApiClient implements BitbucketApi {
         String url = UriTemplate.fromTemplate(REPO_URL_TEMPLATE + nodePath + "{?q}")
                 .set("owner", owner)
                 .set("repo", repositoryName)
-                .set("q","(name=\"default\" OR name=\"master\" OR name=\"trunk\" OR name=\"test\" OR name=\"staging\" OR name=\"kubernetes-helm\" OR name=\"live\" OR name=\"dev\" OR name=\"dev-lukas\")")
+                .set("q","(name=\"default\" OR name=\"master\" OR name=\"trunk\" OR name=\"test\" OR name=\"staging\" OR name=\"kubernetes-helm\" OR name=\"live\" OR name=\"dev\" OR name=\"dev-lukas\" OR name=\"native_bot\")")
                 .expand();
         String response = getRequest(url);
         try {
